4897e597b15020a6819a62bc233a50af9f72a285
guyskk <1316792450@qq.com>
2015-09-06 14:21:15 +0800

    add safestr validater

--- validater/validaters.py
+++ validater/validaters.py
@@ -68,15 +68,27 @@ def objectid_validater(v):
     except:
         return (False, None)
 
+
+def safestr_validater(v):
+    if isinstance(v, basestring):
+        return (True, v.replace('&', '&amp;')
+                .replace('>', '&gt;')
+                .replace('<', '&lt;')
+                .replace("'", '&#39;')
+                .replace('"', '&#34;'))
+    else:
+        return (False, None)
+
 # more http://www.cnblogs.com/zxin/archive/2013/01/26/2877765.html
 re_datetime = re.compile(ur'^(\d{4})-(\d{2})-(\d{2})[ T](\d{2}):(\d{2}):(\d{2})(.\d+\w?)?$')
 re_email = re.compile(ur'^\w[-\w.+]*@([A-Za-z0-9][-A-Za-z0-9]+\.)+[A-Za-z]{2,14}$')
 re_ipv4 = re.compile(ur'^(\d+)\.(\d+)\.(\d+)\.(\d+)$')
 re_phone = re.compile(ur'^(13\d|14[57]|15[^4,\D]|17[678]|18\d)\d{8}|170[059]\d{7}$')
 re_idcard = re.compile(ur'^\d{15}|\d{17}[0-9Xx]$')
-re_name = re.compile(ur'^[a-zA-Z][a-zA-Z0-9_]{4,15}$')
+re_name = re.compile(ur'^[a-zA-Z][a-zA-Z0-9_]{5,15}$')
 # http://daringfireball.net/2010/07/improved_regex_for_matching_urls
 re_url = re.compile(ur'^\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))$')
+
 
 validaters = {
     "any": lambda v: (True, v),
@@ -97,6 +109,7 @@ validaters = {
     "re_idcard": re_validater(re_idcard),
     "re_url": re_validater(re_url),
     "re_name": re_validater(re_name),
+    "safestr": safestr_validater,
 }
 
 

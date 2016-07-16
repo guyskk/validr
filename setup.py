3842999c4a95d2fd013e5b4e1e6b4bdf522b079f
guyskk <guyskk@qq.com>
2016-07-16 23:11:47 +0000

    release v0.10.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name="validater",
-    version="0.10.2",
+    version="0.10.3",
     description="a python tool to "
                 "validate json/dict/list and convert value by schema",
     author="guyskk",

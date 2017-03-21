fdbc9df1cb4234434e54159fc4c6574f4186e83b
kk <guyskk@qq.com>
2017-03-21 13:04:10 +0800

    test warning, fix travis-ci

--- bb.sh
+++ bb.sh
@@ -4,5 +4,5 @@ set -ex
 rm -f validr/*.c
 rm -f validr/*.so
 pip install --upgrade --force-reinstall -e .
-pytest -s
+pytest -s -rw
 python benchmark/benchmark.py benchmark --validr
--- tests/test_deprecated.py
+++ tests/test_deprecated.py
@@ -1,15 +1,17 @@
+import pytest
 from validr import Invalid, SchemaParser
 from validr.validators import handle_default_optional_desc
 
 
 def test_custom_validator():
-    @handle_default_optional_desc()
-    def choice_validator():
-        def validator(value):
-            if value in "ABCD":
-                return value
-            raise Invalid("invalid choice")
-        return validator
+    with pytest.warns(DeprecationWarning):
+        @handle_default_optional_desc()
+        def choice_validator():
+            def validator(value):
+                if value in "ABCD":
+                    return value
+                raise Invalid("invalid choice")
+            return validator
     sp = SchemaParser(validators={"choice": choice_validator})
     for value in "ABCD":
         assert sp.parse("choice")(value) == value
@@ -20,14 +22,15 @@ def test_deprecated():
     from validr.validators import builtin_validators  # noqa
     from validr.validators import build_re_validator  # noqa
     from validr.schema import MarkIndex, MarkKey  # noqa
-    try:
-        with MarkIndex([1, 2, 3]):
-            raise Invalid("test")
-    except Invalid:
-        pass
-
-    try:
-        with MarkKey("key"):
-            raise Invalid("test")
-    except Invalid:
-        pass
+    with pytest.warns(DeprecationWarning):
+        try:
+            with MarkIndex([1, 2, 3]):
+                raise Invalid("test")
+        except Invalid:
+            pass
+    with pytest.warns(DeprecationWarning):
+        try:
+            with MarkKey("key"):
+                raise Invalid("test")
+        except Invalid:
+            pass

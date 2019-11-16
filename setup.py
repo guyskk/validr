fdfc757565b5b672506a0e23d5ab2422a7a76191
guyskk <guyskk@qq.com>
2019-11-16 20:59:44 +0800

    Bump version: 1.1.1 → 1.1.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def _read_file(filepath):
 
 _SETUP_OPTIONS = dict(
     name='validr',
-    version='1.1.1',
+    version='1.1.2',
     keywords='validation validator validate schema jsonschema',
     description=(
         'A simple, fast, extensible python library for data validation.'),

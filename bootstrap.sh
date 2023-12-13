01102c6ad643bec4cbed72f86af8bd23611fe60f
guyskk <guyskk@qq.com>
2023-12-13 18:06:26 +0800

    add on-push github action

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ set -ex
 
 pip install 'pip>=19.1.1'
 pip install 'py>=1.8.0'  # work around pytest requirement
-pip install 'cython>=0.25.2'
+pip install 'cython>=3.0.6'
 VALIDR_SETUP_MODE=dist pip install -e '.[dev,benchmark]' -r requirements.txt
 
 pre-commit install

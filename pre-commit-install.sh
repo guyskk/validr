2b4b4d771d284006535aea8016c56b0239d4be5d
guyskk <guyskk@qq.com>
2023-12-13 23:34:30 +0800

    fix pre-commit

--- pre-commit-install.sh
+++ pre-commit-install.sh
@@ -2,5 +2,6 @@
 set -ex
 
 python -m venv .pre-commit/
-.pre-commit/bin/pip install 'pre-commit==1.21.0'
+.pre-commit/bin/pip install 'pre-commit==3.6.0'
 .pre-commit/bin/pre-commit install
+.pre-commit/bin/pre-commit install-hooks
